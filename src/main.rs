2c40671ec5ab3455395229f812a27b8e4cd9f0fd
Praveen Kumar <praveen+git@kumar.in>
2020-05-27 15:21:40 -0700

    Emit OSC 52 wrapped text from stdin to stdout

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+extern crate base64;
+
+use std::io::{self, Read, Write};
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    let mut handle = stdin.lock();
+
+    handle.read_to_string(&mut buffer)?;
+    let osc_buffer = format!("\x1b]52;0;{}\x07", base64::encode(buffer.as_bytes()));
+    std::io::stdout().write_all(osc_buffer.as_bytes()).unwrap();
+    std::io::stdout().flush().unwrap();
+    Ok(())
 }
